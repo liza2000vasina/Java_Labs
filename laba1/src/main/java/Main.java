21f46b97d178cf510349e6dfccc9e1100bb5bfe2
Liza <58439779+liza2000vasina@users.noreply.github.com>
2019-12-10 13:06:50 +0200

    tas1 commit

--- laba1/src/main/java/Main.java
+++ laba1/src/main/java/Main.java
@@ -6,7 +6,7 @@ import java.util.logging.Logger;
 public class Main {
 
     public static void main(String[] args) throws NoSuchAlgorithmException {
-        int task1 =1;
+        int task1 =2;
         Questions questions = new Questions();
         questions.init();
         Logger.getAnonymousLogger().log(Level.FINE, (Supplier<String>) questions.arrayLists);
